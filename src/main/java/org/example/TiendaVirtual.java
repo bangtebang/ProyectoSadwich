4ea51e5ed915961dcaf8fdac83ff448403df426c
Paulo Vicente <paulovicentetoledosalgado@gmail.com>
2023-06-12 04:47:09 -0400

    Avance Paulo Toledo.

---
+++ src/main/java/org/example/TiendaVirtual.java
@@ -0,0 +1,23 @@
+package org.example;
+public class TiendaVirtual {
+
+	public void generarSandwish() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void agregarUsuario() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void atribuirGustos() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void iniciarInterfases() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void modificarUsuario() {
+		throw new UnsupportedOperationException();
+	}
+}
